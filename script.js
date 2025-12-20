03cb9f2ffd39c4523b319be9a2b577c1dba64d19
Avraneel Pal <avraneelpal22@gmail.com>
2025-12-21 00:52:25 +0530

    Add entry and display functions

--- script.js
+++ script.js
@@ -13,14 +13,22 @@ function game() {
     ];
 
     const addEntry = function (pos, val) {
-        gameboard[pos/3][pos%3] = val
+        gameboard[Math.floor(pos/3)][pos%3] = val;
     }
 
     const displayBoard = function() {
-        console.log(`${gameboard[0]}\n${gameboard[1]}\n${gameboard[2]}`);
+        console.log(
+            `${gameboard[0][0]} ${gameboard[0][1]} ${gameboard[0][2]}\n${gameboard[1][0]} ${gameboard[1][1]} ${gameboard[1][2]}\n${gameboard[2][0]} ${gameboard[2][1]} ${gameboard[2][2]}`
+        );
     }
 
     return {addEntry, displayBoard};
 }
 
 const board = game();
+board.addEntry(0, '0');
+board.addEntry(1, 'x');
+board.addEntry(3, 'x');
+board.addEntry(6, '0');
+board.addEntry(8, 'x');
+board.displayBoard();
