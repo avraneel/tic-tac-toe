d0b7788c0967b7098355a7034449c7e22f007cc1
Avraneel Pal <avraneelpal22@gmail.com>
2025-12-21 22:52:35 +0530

    Add winning logic

--- script.js
+++ script.js
@@ -1,34 +1,96 @@
 "use strict";
 
-function player(name) {
-    return {name};
+function player(name, sign) {
+    return {name, sign};
 }
 
 
 function game() {
-    const gameboard = [
+    const board = [
         [' ', ' ', ' '],
         [' ', ' ', ' '],
         [' ', ' ', ' ']
     ];
 
     const addEntry = function (pos, val) {
-        gameboard[Math.floor(pos/3)][pos%3] = val;
+
+
     }
 
     const displayBoard = function() {
         console.log(
-            `${gameboard[0][0]} ${gameboard[0][1]} ${gameboard[0][2]}\n${gameboard[1][0]} ${gameboard[1][1]} ${gameboard[1][2]}\n${gameboard[2][0]} ${gameboard[2][1]} ${gameboard[2][2]}`
+            `${board[0][0]}|${board[0][1]}|${board[0][2]}\n${board[1][0]}|${board[1][1]}|${board[1][2]}\n${board[2][0]}|${board[2][1]}|${board[2][2]}`
         );
     }
 
-    return {addEntry, displayBoard};
+    const win = function() {
+        if(
+            (board[0][0] == 'x' && board[0][1] == 'x' && board[0][2] == 'x') ||
+            (board[1][0] == 'x' && board[1][1] == 'x' && board[1][2] == 'x') ||
+            (board[2][0] == 'x' && board[2][1] == 'x' && board[2][2] == 'x') ||
+            (board[0][0] == 'x' && board[1][0] == 'x' && board[2][0] == 'x') ||
+            (board[0][1] == 'x' && board[1][1] == 'x' && board[2][1] == 'x') ||
+            (board[0][2] == 'x' && board[1][2] == 'x' && board[2][2] == 'x') ||
+            (board[0][0] == 'x' && board[1][1] == 'x' && board[2][2] == 'x') ||
+            (board[0][2] == 'x' && board[1][1] == 'x' && board[2][0] == 'x')
+        ) {
+            console.log("Player 1 wins!");
+            return 1;
+        } 
+        else if(
+            (board[0][0] == '0' && board[0][1] == '0' && board[0][2] == '0') ||
+            (board[1][0] == '0' && board[1][1] == '0' && board[1][2] == '0') ||
+            (board[2][0] == '0' && board[2][1] == '0' && board[2][2] == '0') ||
+            (board[0][0] == '0' && board[1][0] == '0' && board[2][0] == '0') ||
+            (board[0][1] == '0' && board[1][1] == '0' && board[2][1] == '0') ||
+            (board[0][2] == '0' && board[1][2] == '0' && board[2][2] == '0') ||
+            (board[0][0] == '0' && board[1][1] == '0' && board[2][2] == '0') ||
+            (board[0][2] == '0' && board[1][1] == '0' && board[2][0] == '0')
+        ) {
+            console.log("Player 2 wins!");
+            return 2;
+        }
+        else {
+            console.log("Else case");
+            return 0;
+        }
+    }
+
+    const runGame = function() {
+        let flag = 0;
+        let filled = 0
+
+        while(filled < 9) {
+            const pos = prompt("Enter pos");
+            if(pos > 8) {
+                alert("pos must be within range");
+            }
+            const val = prompt("Enter val");
+            const row = Math.floor(pos/3);
+            const col = pos%3;
+            if(board[row][col] == 'x' || board[row][col] == '0') {
+                // Check if cell is already filled
+                alert("Can't re-enter in a value that is already filled");
+                continue;
+            }
+            else {
+                board[row][col] = val;
+                filled++;
+            }
+            displayBoard();
+            if(win() != 0) {
+                flag = 1;
+                break;
+            }
+            console.log(`Round ${filled} finished`);
+        }
+        if(flag == 0) {
+            console.log("It's a draw");
+        } 
+    }
+    return {runGame};
 }
 
 const board = game();
-board.addEntry(0, '0');
-board.addEntry(1, 'x');
-board.addEntry(3, 'x');
-board.addEntry(6, '0');
-board.addEntry(8, 'x');
-board.displayBoard();
+board.runGame();
+
