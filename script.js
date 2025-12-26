99f33f576e3642fe2213f75ca0968921b7a093db
Avraneel Pal <avraneelpal22@gmail.com>
2025-12-26 23:13:51 +0530

    implement one turn

--- script.js
+++ script.js
@@ -14,15 +14,12 @@ const gameboard = (
         ];
 
         const setBoardValue = (pos, val) => {
-            if(pos > 8) {
-                return 1;
-            }
             const row = Math.floor(pos/3);
             const col = pos%3;
             // Check if cell is already filled
             if(board[row][col] != "") {
                 console.log(`Cell ${pos} is already filled!`);
-                return 2;
+                return 1;
             }
             else {
                 board[row][col] = val;
@@ -40,38 +37,40 @@ const gameboard = (
 )(); // Board IIFE ends here
 
 function game() {
-    const player1 = player("x");
-    const player2 = player("O");
+    const playerX = player("x");
+    const playerO = player("O");
+
+    let currentPlayer = playerX;
 
     // Function to check End Conditions
     const checkEnd = function() {
         const board = gameboard.board;
         // Case 1: Player 1 wins
         if(
-            (board[0][0] == player1.sign && board[0][1] == player1.sign && board[0][2] == player1.sign) ||
-            (board[1][0] == player1.sign && board[1][1] == player1.sign && board[1][2] == player1.sign) ||
-            (board[2][0] == player1.sign && board[2][1] == player1.sign && board[2][2] == player1.sign) ||
-            (board[0][0] == player1.sign && board[1][0] == player1.sign && board[2][0] == player1.sign) ||
-            (board[0][1] == player1.sign && board[1][1] == player1.sign && board[2][1] == player1.sign) ||
-            (board[0][2] == player1.sign && board[1][2] == player1.sign && board[2][2] == player1.sign) ||
-            (board[0][0] == player1.sign && board[1][1] == player1.sign && board[2][2] == player1.sign) ||
-            (board[0][2] == player1.sign && board[1][1] == player1.sign && board[2][0] == player1.sign)
+            (board[0][0] == playerX.sign && board[0][1] == playerX.sign && board[0][2] == playerX.sign) ||
+            (board[1][0] == playerX.sign && board[1][1] == playerX.sign && board[1][2] == playerX.sign) ||
+            (board[2][0] == playerX.sign && board[2][1] == playerX.sign && board[2][2] == playerX.sign) ||
+            (board[0][0] == playerX.sign && board[1][0] == playerX.sign && board[2][0] == playerX.sign) ||
+            (board[0][1] == playerX.sign && board[1][1] == playerX.sign && board[2][1] == playerX.sign) ||
+            (board[0][2] == playerX.sign && board[1][2] == playerX.sign && board[2][2] == playerX.sign) ||
+            (board[0][0] == playerX.sign && board[1][1] == playerX.sign && board[2][2] == playerX.sign) ||
+            (board[0][2] == playerX.sign && board[1][1] == playerX.sign && board[2][0] == playerX.sign)
         ) {
-            console.log("Player 1 wins!");
+            console.log("Player X wins!");
             return 1;
         } 
         // Case 2: Player 2 wins
         else if(
-            (board[0][0] == player2.sign && board[0][1] == player2.sign && board[0][2] == player2.sign) ||
-            (board[1][0] == player2.sign && board[1][1] == player2.sign && board[1][2] == player2.sign) ||
-            (board[2][0] == player2.sign && board[2][1] == player2.sign && board[2][2] == player2.sign) ||
-            (board[0][0] == player2.sign && board[1][0] == player2.sign && board[2][0] == player2.sign) ||
-            (board[0][1] == player2.sign && board[1][1] == player2.sign && board[2][1] == player2.sign) ||
-            (board[0][2] == player2.sign && board[1][2] == player2.sign && board[2][2] == player2.sign) ||
-            (board[0][0] == player2.sign && board[1][1] == player2.sign && board[2][2] == player2.sign) ||
-            (board[0][2] == player2.sign && board[1][1] == player2.sign && board[2][0] == player2.sign)
+            (board[0][0] == playerO.sign && board[0][1] == playerO.sign && board[0][2] == playerO.sign) ||
+            (board[1][0] == playerO.sign && board[1][1] == playerO.sign && board[1][2] == playerO.sign) ||
+            (board[2][0] == playerO.sign && board[2][1] == playerO.sign && board[2][2] == playerO.sign) ||
+            (board[0][0] == playerO.sign && board[1][0] == playerO.sign && board[2][0] == playerO.sign) ||
+            (board[0][1] == playerO.sign && board[1][1] == playerO.sign && board[2][1] == playerO.sign) ||
+            (board[0][2] == playerO.sign && board[1][2] == playerO.sign && board[2][2] == playerO.sign) ||
+            (board[0][0] == playerO.sign && board[1][1] == playerO.sign && board[2][2] == playerO.sign) ||
+            (board[0][2] == playerO.sign && board[1][1] == playerO.sign && board[2][0] == playerO.sign)
         ) {
-            console.log("Player 2 wins!");
+            console.log("Player O wins!");
             return 2;
         }
         // Case 3: It's a draw if no cell is empty
@@ -85,70 +84,36 @@ function game() {
         }
     }
 
-    const runGame = () => {
-        gameboard.setBoardValue(0, player1.sign);
-        checkEnd();
-        gameboard.getBoard();
-        gameboard.setBoardValue(1, player2.sign);
-        checkEnd();
-        gameboard.getBoard();
-        gameboard.setBoardValue(2, player1.sign);
-        checkEnd();
-        gameboard.getBoard();
-        gameboard.setBoardValue(3, player2.sign);
-        checkEnd();
-        gameboard.getBoard();
-        gameboard.setBoardValue(4, player1.sign);
-        checkEnd();
-        gameboard.getBoard();
-        gameboard.setBoardValue(5, player2.sign);
-        checkEnd();
-        gameboard.getBoard();
-        gameboard.setBoardValue(7, player1.sign);
-        checkEnd();
-        gameboard.getBoard();
-        gameboard.setBoardValue(6, player2.sign);
-        checkEnd();
-        gameboard.getBoard();
-        gameboard.setBoardValue(8, player2.sign);
-        checkEnd();
-        gameboard.getBoard();
+    const doATurn = function(pos) {
+        console.log(`Player ${currentPlayer.sign} turn`);
+        gameboard.setBoardValue(pos, currentPlayer.sign);
+        if(checkEnd()) {
+            return;
+        }
 
+        // Switching players
+        currentPlayer = currentPlayer == playerX ? playerO : playerX;
     }
-    return {runGame};
+
+    return {doATurn};
 }
 
 // IIFE: A single display controllers handles I/O stuff
 const displayController = (
     function() {
         const cells = document.querySelectorAll(".cell");
-
+        const g = game();
         // In this factory function, we attach the 'click' event to each
         // cell of the board
         for(let i = 0; i < cells.length; i++) {
-            cells[i].addEventListener('click', function(playerobj) {
-                if(!gameboard.setBoardValue(i, playerobj.sign))
-                    cells[i].textContent = "L";
+            cells[i].addEventListener('click', function() {
+                // This will update the internal board array
+                g.doATurn(i);
+                // Now render the updated board value
+                cells[i].textContent = gameboard.board[Math.floor(i/3)][i%3];
             })
         }
-
-        const renderBoard = () => {
-            const board = [
-                        ["x", "0", "x"],
-                        ["0", "0", "x"],
-                        ["0", "", "0"]
-                    ];
-            for(let i = 0; i < cells.length; i++) {
-                cells[i].textContent = board[Math.floor(i/3)][i%3];
-            }
-        }
-
-        // When we return the object, the cells corresponding to the Display Controller object
-        // will update their values on click
-        return {renderBoard};
     }
 )();
 
 const g = game();
-
-displayController.renderBoard();
